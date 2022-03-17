5ce403d10a82c28c464245ae003172923924a750
Бакланов Илья Андреевич (4054509) <ibaklanov@dev.vtb.ru>
2022-03-17 22:48:18 +0300

    feat: add new string

--- calculator/script.js
+++ calculator/script.js
@@ -1,39 +1,39 @@
 const numbers = document.querySelectorAll('.number'),
-      operatorBtns = document.querySelectorAll('.operator'),
-      clearBtns = document.querySelectorAll('.clear-btn'),
-      decimalBtn = document.getElementById('decimal'),
-      display = document.getElementById('display'),
-      squareRootBtn = document.getElementById('square-root'),
-      percentBtn = document.getElementById('percent'),
-      factorialBtn = document.getElementById('factorial'),
-      squareBtn = document.getElementById('square'),
-      instructionBtn = document.getElementById('instruction');
+    operatorBtns = document.querySelectorAll('.operator'),
+    clearBtns = document.querySelectorAll('.clear-btn'),
+    decimalBtn = document.getElementById('decimal'),
+    display = document.getElementById('display'),
+    squareRootBtn = document.getElementById('square-root'),
+    percentBtn = document.getElementById('percent'),
+    factorialBtn = document.getElementById('factorial'),
+    squareBtn = document.getElementById('square'),
+    instructionBtn = document.getElementById('instruction');
 let MemoryCurrentNumber = 0,
     MemoryNewNumber = false,
     MemoryPandingOperation = '';
 
-for(let i = 0; i < numbers.length; i++){
+for (let i = 0; i < numbers.length; i++) {
     let number = numbers[i];
     number.addEventListener('click', function (e) {
         numberPress(e.target.textContent)
     })
 };
 
-for(let i = 0; i < operatorBtns.length; i++){
+for (let i = 0; i < operatorBtns.length; i++) {
     let operatorBtn = operatorBtns[i];
     operatorBtn.addEventListener('click', function (e) {
         operation(e.target.textContent)
     })
 };
 
-for(let i = 0; i < clearBtns.length; i++){
+for (let i = 0; i < clearBtns.length; i++) {
     let clearBtn = clearBtns[i];
-    clearBtn.addEventListener('click', function(e) {
+    clearBtn.addEventListener('click', function (e) {
         clear(e.target.textContent)
     })
 };
 
-decimalBtn.addEventListener('click', decimal);   
+decimalBtn.addEventListener('click', decimal);
 squareRootBtn.addEventListener('click', squareRoot);
 percentBtn.addEventListener('click', percent);
 factorialBtn.addEventListener('click', factorial);
@@ -59,29 +59,29 @@ function instruction(params) {
 }
 
 function numberPress(number) {
-    if(MemoryPandingOperation === '='){
+    if (MemoryPandingOperation === '=') {
         display.value = number;
         MemoryNewNumber = false;
         MemoryCurrentNumber = 0;
         MemoryPandingOperation = '';
     } else {
-        if(MemoryNewNumber) {
+        if (MemoryNewNumber) {
             display.value = number;
             MemoryNewNumber = false;
         } else {
-            if(display.value === '0'){
+            if (display.value === '0') {
                 display.value = number;
             } else {
                 display.value += number;
             }
         }
     }
-    
+
 }
 
 function operation(op) {
-    if(op === '-' && display.value == 0){
-        if(MemoryPandingOperation === '='){
+    if (op === '-' && display.value == 0) {
+        if (MemoryPandingOperation === '=') {
             display.value = '-';
             MemoryNewNumber = false;
             MemoryCurrentNumber = 0;
@@ -91,67 +91,67 @@ function operation(op) {
             MemoryNewNumber = false;
         };
     } else {
-        if(MemoryCurrentNumber === 0){
+        if (MemoryCurrentNumber === 0) {
             MemoryCurrentNumber = +display.value;
             display.value = '0';
         } else {
             MemoryNewNumber = true;
-            if(MemoryPandingOperation === '+'){
+            if (MemoryPandingOperation === '+') {
                 MemoryCurrentNumber = MemoryCurrentNumber + +display.value;
                 display.value = +MemoryCurrentNumber.toFixed(7);
                 MemoryPandingOperation = '';
-            } else if(MemoryPandingOperation === '-'){
+            } else if (MemoryPandingOperation === '-') {
                 MemoryCurrentNumber = MemoryCurrentNumber - +display.value;
                 display.value = +MemoryCurrentNumber.toFixed(7);
                 MemoryPandingOperation = '';
-            } else if(MemoryPandingOperation === '*'){
+            } else if (MemoryPandingOperation === '*') {
                 MemoryCurrentNumber = MemoryCurrentNumber * +display.value;
                 display.value = +MemoryCurrentNumber.toFixed(7);
                 MemoryPandingOperation = '';
-            } else if(MemoryPandingOperation === '/'){
+            } else if (MemoryPandingOperation === '/') {
                 MemoryCurrentNumber = MemoryCurrentNumber / +display.value;
-                    if(MemoryCurrentNumber === Infinity){
-                        display.value = 'Ах ты ж хулюган!';
-                    } else {
+                if (MemoryCurrentNumber === Infinity) {
+                    display.value = 'Ах ты ж хулюган!';
+                } else {
                     display.value = +MemoryCurrentNumber.toFixed(7);
                     MemoryPandingOperation = '';
-                    }
-            } else if(MemoryPandingOperation === 'X^'){
+                }
+            } else if (MemoryPandingOperation === 'X^') {
                 MemoryCurrentNumber = MemoryCurrentNumber ** +display.value;
                 display.value = +MemoryCurrentNumber.toFixed(7);
                 MemoryPandingOperation = '';
-            } else if(op === '=' && MemoryPandingOperation === '='){
+            } else if (op === '=' && MemoryPandingOperation === '=') {
                 MemoryCurrentNumber = 0;
                 display.value = '0';
             } else {
                 display.value = '0';
             }
-        } 
-    
+        }
+
         MemoryPandingOperation = op;
     }
 }
 
 function decimal(params) {
     let localDecimalMemory = display.value;
-    
+
     if (MemoryNewNumber) {
         localDecimalMemory = '0.';
         MemoryNewNumber = false;
     } else {
-        if(localDecimalMemory.indexOf('.') === -1) {
+        if (localDecimalMemory.indexOf('.') === -1) {
             localDecimalMemory += '.';
         }
-        
+
     }
-    display.value = localDecimalMemory; 
-} 
+    display.value = localDecimalMemory;
+}
 
 function squareRoot(params) {
-    if(display.value < 0){
+    if (display.value < 0) {
         display.value = 'Это фиаско, Братан!';
     } else {
-        if(MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')){
+        if (MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')) {
             MemoryCurrentNumber = Math.sqrt(display.value);
             display.value = MemoryCurrentNumber;
             MemoryPandingOperation = '';
@@ -162,47 +162,47 @@ function squareRoot(params) {
     }
 }
 
-function percent (params) {
-    display.value = (MemoryCurrentNumber / 100) * Number(display.value) ;
+function percent(params) {
+    display.value = (MemoryCurrentNumber / 100) * Number(display.value);
 }
 
-function factorial(){
-    if(display.value < 0 || display.value > 15){
+function factorial() {
+    if (display.value < 0 || display.value > 15) {
         alert('Введите число от 0 до 15');
-    } else if(MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')){
-        if(display.value == 0){
+    } else if (MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')) {
+        if (display.value == 0) {
             MemoryCurrentNumber = 1;
             display.value = MemoryCurrentNumber;
             MemoryNewNumber = true;
             MemoryPandingOperation = '';
         } else {
-        let fact = 1;
-            for(let i = display.value; i > 0; i--) {
-          fact *= i
-        }
-        MemoryCurrentNumber = fact
-        display.value  = MemoryCurrentNumber;
-        MemoryNewNumber = true;
-        MemoryPandingOperation = '';
+            let fact = 1;
+            for (let i = display.value; i > 0; i--) {
+                fact *= i
+            }
+            MemoryCurrentNumber = fact
+            display.value = MemoryCurrentNumber;
+            MemoryNewNumber = true;
+            MemoryPandingOperation = '';
         }
     } else {
-        if(display.value == 0){
+        if (display.value == 0) {
             display.value = 1;
             MemoryNewNumber = true;
         } else {
-        let fact = 1;
-            for(let i = display.value; i > 0; i--) {
-          fact *= i
+            let fact = 1;
+            for (let i = display.value; i > 0; i--) {
+                fact *= i
+            }
+            display.value = fact;
+            MemoryNewNumber = true;
         }
-        display.value  = fact;
-        MemoryNewNumber = true;
-        }        
-    } 
-    
+    }
+
 }
 
 function square(params) {
-    if(MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')){
+    if (MemoryPandingOperation === '=' || (MemoryCurrentNumber !== 0 && MemoryPandingOperation === '')) {
         MemoryCurrentNumber = display.value * display.value;
         display.value = MemoryCurrentNumber;
         MemoryPandingOperation = '';
@@ -213,7 +213,7 @@ function square(params) {
 }
 
 function clear(id) {
-    if (id === 'ce'){
+    if (id === 'ce') {
         display.value = '0';
         MemoryNewNumber = true;
     } else if (id === 'c') {
@@ -228,3 +228,5 @@ function clear(id) {
         display.value = '0';
     }
 }
+
+'add new string' + 'also new string'
